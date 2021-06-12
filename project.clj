fbbceb22f61f8f847b9f271cb6e26b1a0b6ee4cc
Rune Juhl Jacobsen <runejuhl@petardo.dk>
2021-06-12 17:06:38 +0200

    Bump cljfmt to 0.7.0

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [cljfmt "0.6.6"]]
+                 [cljfmt "0.7.0"]]
   :main cljfmt-graalvm.core
   :uberjar-name "cljfmt-graalvm-standalone.jar"
   :profiles {:uberjar {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]
                        :aot      :all}}
-  :plugins [[com.roomkey/lein-v "6.1.0"
+  :plugins [[com.roomkey/lein-v "7.2.0"
              :middleware true
              :hooks      true]]
   :middleware [lein-v.plugin/middleware]
